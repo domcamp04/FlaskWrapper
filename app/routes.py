2f30dd6ffbc95133085fdc3fa88bbe7e687ae134
Brian <bstanton773@gmail.com>
2021-11-09 11:45:57 -0600

    accept form submit

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,10 @@ from flask import render_template
 from app.forms import TVSearchForm
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     form = TVSearchForm()
+    if form.validate_on_submit():
+        show = form.title.data
+        print('Form Submitted!', show)
     return render_template('index.html', form=form)
