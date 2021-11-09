322e390bd4f59a46ad1c16f01ef816a5592437b1
Brian <bstanton773@gmail.com>
2021-11-09 11:55:22 -0600

    Add wrapper to route and index.html

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,15 @@
 from app import app
 from flask import render_template
 from app.forms import TVSearchForm
+from app.wrappers import TVMazeAPI
 
+client = TVMazeAPI()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = TVSearchForm()
+    show_list = None
     if form.validate_on_submit():
         show = form.title.data
-        print('Form Submitted!', show)
-    return render_template('index.html', form=form)
+        show_list = client.search_shows(show)
+    return render_template('index.html', form=form, show_list=show_list)
